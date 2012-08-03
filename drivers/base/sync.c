b98bbb25a2eea8c1032e5051de613281ad818b2f
Ajay Dudani <adudani@codeaurora.org>
2012-08-02 17:26:45 -0700

    sync: add reference counting to timelines
    If a timeline is destroyed while fences still hold pts on it, the reworked
    fence release handler can cause the timeline to be freed before all it's points
    are freed.
    
    Change-Id: I1cd8ddb638eded7db9db446ff6b37f3dd165d6c4
    Signed-off-by: Erik Gilling <konkers@android.com>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>
    
    Conflicts:
    
    	drivers/base/sync.c

--- drivers/base/sync.c
+++ drivers/base/sync.c
@@ -50,6 +50,7 @@ struct sync_timeline *sync_timeline_create(const struct sync_timeline_ops *ops,
 	if (obj == NULL)
 		return NULL;
 
+	kref_init(&obj->kref);
 	obj->ops = ops;
 	strlcpy(obj->name, name, sizeof(obj->name));
 
@@ -67,8 +68,10 @@ struct sync_timeline *sync_timeline_create(const struct sync_timeline_ops *ops,
 }
 EXPORT_SYMBOL(sync_timeline_create);
 
-static void sync_timeline_free(struct sync_timeline *obj)
+static void sync_timeline_free(struct kref *kref)
 {
+	struct sync_timeline *obj =
+		container_of(kref, struct sync_timeline, kref);
 	unsigned long flags;
 
 	if (obj->ops->release_obj)
@@ -86,14 +89,14 @@ void sync_timeline_destroy(struct sync_timeline *obj)
 	unsigned long flags;
 	bool needs_freeing;
 
-	spin_lock_irqsave(&obj->child_list_lock, flags);
 	obj->destroyed = true;
-	needs_freeing = list_empty(&obj->child_list_head);
-	spin_unlock_irqrestore(&obj->child_list_lock, flags);
 
-	if (needs_freeing)
-		sync_timeline_free(obj);
-	else
+	/*
+	 * If this is not the last reference, signal any children
+	 * that their parent is going away.
+	 */
+
+	if (!kref_put(&obj->kref, sync_timeline_free))
 		sync_timeline_signal(obj);
 }
 EXPORT_SYMBOL(sync_timeline_destroy);
@@ -121,12 +124,10 @@ static void sync_timeline_remove_pt(struct sync_pt *pt)
 	spin_unlock_irqrestore(&obj->active_list_lock, flags);
 
 	spin_lock_irqsave(&obj->child_list_lock, flags);
-	list_del(&pt->child_list);
-	needs_freeing = obj->destroyed && list_empty(&obj->child_list_head);
+	if (!list_empty(&pt->child_list)) {
+		list_del_init(&pt->child_list);
+	}
 	spin_unlock_irqrestore(&obj->child_list_lock, flags);
-
-	if (needs_freeing)
-		sync_timeline_free(obj);
 }
 
 void sync_timeline_signal(struct sync_timeline *obj)
@@ -169,6 +170,7 @@ struct sync_pt *sync_pt_create(struct sync_timeline *parent, int size)
 		return NULL;
 
 	INIT_LIST_HEAD(&pt->active_list);
+	kref_get(&parent->kref);
 	sync_timeline_add_pt(parent, pt);
 
 	return pt;
@@ -181,6 +183,8 @@ void sync_pt_free(struct sync_pt *pt)
 		pt->parent->ops->free_pt(pt);
 
 	sync_timeline_remove_pt(pt);
+
+	kref_put(&pt->parent->kref, sync_timeline_free);
 
 	kfree(pt);
 }
