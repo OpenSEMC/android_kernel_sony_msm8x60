3466472c1ebd605f0ab2d70d845715fa7a2904c7
Carl Vanderlip <carlv@codeaurora.org>
2012-08-14 19:00:22 -0700

    video: msm: Move histogram disable to before panel off
    Histogram expects the display to be on while running, thus moving its
    disable call to before disabling the panel timing generator ensures this
    assertion.
    
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    (cherry picked from commit 67ddb9b38b1793c297ff431ef9f650f355c4abf2)
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    	drivers/video/msm/mdp4_overlay_lcdc.c
    
    Change-Id: I308b3e0c24f0111fb8549d0a0ae025910d649b75
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    	drivers/video/msm/mdp4_overlay_lcdc.c

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -713,21 +713,6 @@ int mdp4_dsi_video_off(struct platform_device *pdev)
 
 	dsi_video_enabled = 0;
 
-	if (vctrl->vsync_irq_enabled) {
-		vctrl->vsync_irq_enabled = 0;
-		vsync_irq_disable(INTR_PRIMARY_VSYNC, MDP_PRIM_VSYNC_TERM);
-	}
-
-	undx =  vctrl->update_ndx;
-	vp = &vctrl->vlist[undx];
-	if (vp->update_cnt) {
-		/*
-		 * pipe's iommu will be freed at next overlay play
-		 * and iommu_drop statistic will be increased by one
-		 */
-		vp->update_cnt = 0;     /* empty queue */
-	}
-
 	undx =  vctrl->update_ndx;
 	vp = &vctrl->vlist[undx];
 	if (vp->update_cnt) {
