fc2602734b53f767efd66a67f704ce5bb25fb559
Kuogee Hsieh <khsieh@codeaurora.org>
2012-10-13 17:26:31 -0700

    msm_fb: display: add video mode dcs cmdlist support
    Add video mode dcs command list support. This patch
    make sure dcs commands committed while dsi video engine
    is busy so that dcs command will be transmitted at beginning
    of next BLLP.
    
    Change-Id: I11b0214ee10f3b1ef33b1da4c8e577800068cb06
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mipi_dsi_host.c

--- drivers/video/msm/mipi_dsi_host.c
+++ drivers/video/msm/mipi_dsi_host.c
@@ -1474,7 +1474,26 @@ int mipi_dsi_cmd_dma_rx(struct dsi_buf *rp, int rlen)
 	return rlen;
 }
 
-static void mipi_dsi_video_wait_to_mdp_busy(void)
+static void mipi_dsi_wait_for_video_eng_busy(void)
+{
+	int i;
+	u32 data;
+
+	/*
+	 * if video mode engine was not busy (in BLLP)
+	 * wait to pass BLLP
+	 */
+
+	/* check for VIDEO_MODE_ENGINE_BUSY */
+	for (i = 0; i < 5; i++) {
+		data = MIPI_INP(MIPI_DSI_BASE + 0x0004);/* DSI_STATUS */
+		if (data & 0x08) /* VIDEO_MODE_ENGINE_BUSY */
+			break;
+		usleep(4000);
+	}
+}
+
+void mipi_dsi_cmd_mdp_busy(void)
 {
 	unsigned long flags;
 	int need_wait = 0;
@@ -1549,6 +1568,7 @@ void mipi_dsi_cmdlist_commit(int from_mdp)
 {
 	struct dcs_cmd_req *req;
 	int video;
+	u32 dsi_ctrl;
 
 	mutex_lock(&cmd_mutex);
 	req = mipi_dsi_cmdlist_get();
@@ -1561,23 +1581,12 @@ void mipi_dsi_cmdlist_commit(int from_mdp)
 
 	pr_debug("%s:  from_mdp=%d pid=%d\n", __func__, from_mdp, current->pid);
 
-	if (req == NULL)
-		goto need_lock;
-
-	video = MIPI_INP(MIPI_DSI_BASE + 0x0000);
-	video &= 0x02; /* VIDEO_MODE */
-
-	if (!video)
-		mipi_dsi_clk_cfg(1);
-
-	pr_debug("%s:  from_mdp=%d pid=%d\n", __func__, from_mdp, current->pid);
-
 	dsi_ctrl = MIPI_INP(MIPI_DSI_BASE + 0x0000);
 	if (dsi_ctrl & 0x02) {
 		/* video mode, make sure dsi_cmd_mdp is busy
-		 * so dcs command will be txed at start of BLLP
+		 * sodcs command will be txed at start of BLLP
 		 */
-		mipi_dsi_video_wait_to_mdp_busy();
+		mipi_dsi_wait_for_video_eng_busy();
 	} else {
 		/* command mode */
 		if (!from_mdp) { /* cmdlist_put */
