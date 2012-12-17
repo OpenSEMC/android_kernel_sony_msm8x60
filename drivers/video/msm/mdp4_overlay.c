30e3caa21a0b02428455724612492d2560e82ea2
Shruthi Krishna <skrish@codeaurora.org>
2012-12-17 14:58:08 -0800

    msm_fb: display: wfd with vsync driven frame push
    To improve performance, hardware vsync event at mdp driver
    is passed to framework so that both composition and frame
    update are aligned at vsync event. This patch will upgrade
    wfd to incorporate vsync driven frame push in consistence
    with other interfaces.
    
    Change-Id: If088826ec387446301f1f6acc311d909edb04163
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4.h
    	drivers/video/msm/mdp4_overlay_writeback.c
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c
    	drivers/video/msm/mdp4_overlay_writeback.c

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3428,8 +3428,10 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 
 	mdp4_overlay_mdp_perf_req(mfd, ctrl->plist);
 
+/* FIXME: MDDI!!
 	if (pipe->mixer_num == MDP4_MIXER2 || ctrl->panel_mode & MDP4_PANEL_MDDI)
 		goto mddi;
+*/
 
 	if (pipe->mixer_num == MDP4_MIXER0) {
 		if (ctrl->panel_mode & MDP4_PANEL_DSI_CMD) {
@@ -3445,16 +3447,16 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 	} else if (pipe->mixer_num == MDP4_MIXER1) {
 		if (ctrl->panel_mode & MDP4_PANEL_DTV)
 			mdp4_dtv_pipe_queue(0, pipe);/* cndx = 0 */
-	}
 
+/* FIXME: MDDI!!
 	mutex_unlock(&mfd->dma->ov_mutex);
 	return ret;
 
 mddi:
 	if (pipe->pipe_type == OVERLAY_TYPE_VIDEO) {
-		mdp4_overlay_vg_setup(pipe);    /* video/graphic pipe */
+		mdp4_overlay_vg_setup(pipe);    // video/graphic pipe
 	} else {
-		mdp4_overlay_rgb_setup(pipe);	/* rgb pipe */
+		mdp4_overlay_rgb_setup(pipe);	// rgb pipe
 	}
 
 	mdp4_mixer_stage_up(pipe, 0);
@@ -3476,6 +3478,12 @@ mddi:
 		mdp4_mixer_stage_commit(pipe->mixer_num);
 		mdp4_mddi_dma_busy_wait(mfd);
 		mdp4_mddi_kickoff_video(mfd, pipe);
+*/
+
+	} else if (pipe->mixer_num == MDP4_MIXER2) {
+		ctrl->mixer2_played++;
+		if (ctrl->panel_mode & MDP4_PANEL_WRITEBACK)
+			mdp4_wfd_pipe_queue(0, pipe);/* cndx = 0 */
 	}
 
 	/* write out DPP HSIC registers */
