6b75b74cdeabdfdb082e03b41a5e01950531025e
Carl Vanderlip <carlv@codeaurora.org>
2013-01-09 16:47:27 -0800

    video: msm: Unlock mutex before update notify
    When update notify functions holds the entry_mutex, it blocks other MDP
    ioctls from progressing causing a system freeze. Unlock the lock before
    calling to prevent this.
    
    Change-Id: I146b0a92ec844d90f4a8688bd77a38e298c2b9c0
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -4060,7 +4060,9 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 		if (ret)
 			goto msm_fb_ioctl_exit;
 
+		mutex_unlock(&mfd->entry_mutex);
 		ret = mfd->do_histogram(info, &hist);
+		goto exit;
 		break;
 
 	case MSMFB_HISTOGRAM_START:
@@ -4105,7 +4107,9 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 		break;
 
 	case MSMFB_NOTIFY_UPDATE:
+		mutex_unlock(&mfd->entry_mutex);
 		ret = msmfb_notify_update(info, argp);
+		goto exit;
 		break;
 
 	case MSMFB_SET_PAGE_PROTECTION:
@@ -4174,6 +4178,7 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 	}
 msm_fb_ioctl_exit:
 	mutex_unlock(&mfd->entry_mutex);
+exit:
 	return ret;
 }
 
