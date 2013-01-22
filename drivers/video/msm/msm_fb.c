2693e8f974c260381245799064b788321b674e20
Carl Vanderlip <carlv@codeaurora.org>
2013-01-21 18:07:37 -0800

    video: msm: Fix copyright attribution.
    The copyright of these files should be attributed to the Linux Foundation.
    
    Change-Id: I562447ed6e8ba500d8f68598d0a791bd4a1519c2
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -5,7 +5,7 @@
  *
  * Copyright (C) 2007 Google Incorporated
  * Copyright (C) 2012 Sony Mobile Communications AB.
- * Copyright (c) 2008-2013, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2008-2013, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
