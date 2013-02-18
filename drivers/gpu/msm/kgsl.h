1cba3cf01d4eca3e3c59dcd861292028b802d8d8
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-02-19 00:54:22 +0100

    gpu/msm: bulk update kgsl from mako
    Change-Id: Ic81558ae82ae50506ac35fdc51290259dfedf4ef
    
    Conflicts:
    
    	drivers/gpu/msm/adreno_drawctxt.c
    	drivers/gpu/msm/kgsl.c
    	drivers/gpu/msm/kgsl_mmu.h

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -23,6 +23,8 @@
 #include <linux/regulator/consumer.h>
 #include <linux/mm.h>
 
+#include <mach/kgsl.h>
+
 #define KGSL_NAME "kgsl"
 
 /* The number of memstore arrays limits the number of contexts allowed.
@@ -171,6 +173,7 @@ struct kgsl_mem_entry {
 #endif
 
 void kgsl_mem_entry_destroy(struct kref *kref);
+int kgsl_postmortem_dump(struct kgsl_device *device, int manual);
 
 struct kgsl_mem_entry *kgsl_get_mem_entry(unsigned int ptbase,
 		unsigned int gpuaddr, unsigned int size);
