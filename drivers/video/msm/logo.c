21c0c70fc32c3fe66601accb8b806e105a9c84f9
goodwin_c <good.win.alexs@gmail.com>
2013-02-21 17:26:01 +0200

    fix bootup logo

--- drivers/video/msm/logo.c
+++ drivers/video/msm/logo.c
@@ -27,6 +27,7 @@
 #define fb_width(fb)	((fb)->var.xres)
 #define fb_height(fb)	((fb)->var.yres)
 #define fb_size(fb)	((fb)->var.xres * (fb)->var.yres * 2)
+#define INIT_IMAGE_FILE "/logo.rle"
 
 static void memset16(void *_ptr, unsigned short val, unsigned count)
 {
