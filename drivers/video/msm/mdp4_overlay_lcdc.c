156f6b71dda7d71f24a64d6562e9de58395fe76b
RaymanFX <raymanfx@gmail.com>
2013-04-01 20:20:00 +0200

    msm_fb: display: turn vsync irq off at suspend
    Turn off vsycn irq at suspend to prevent vsync irq firing
    before vsycn enabled ioctl called at resume.
    
    Change-Id: Ib7e19dc1e18762fa3b57e19af34c3ef000c08d95

--- drivers/video/msm/mdp4_overlay_lcdc.c
+++ drivers/video/msm/mdp4_overlay_lcdc.c
@@ -703,6 +703,11 @@ int mdp4_lcdc_off(struct platform_device *pdev)
 
 	mdp_histogram_ctrl_all(FALSE);
 
+        if (vctrl->vsync_irq_enabled) {
+            vctrl->vsync_irq_enabled = 0;
+            vsync_irq_disable(INTR_PRIMARY_VSYNC, MDP_PRIM_VSYNC_TERM);
+        }
+
 	undx =  vctrl->update_ndx;
 	vp = &vctrl->vlist[undx];
 	if (vp->update_cnt) {
