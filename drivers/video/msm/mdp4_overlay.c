9ba9a450fa5769678c56a19005662af2cb32773d
RaymanFX <raymanfx@gmail.com>
2013-07-28 17:39:17 +0200

    video: msm: comment HSIC dependant code
    Change-Id: I4f88845bc39518cff3c1626edf73d74ed872b828

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -439,7 +439,6 @@ void fill_black_screen(bool on, uint8 pipe_num, uint8 mixer_num)
 
 	return;
 }
-#endif
 
 void mdp4_overlay_dmae_xy(struct mdp4_overlay_pipe *pipe)
 {
@@ -3491,9 +3490,12 @@ mddi:
 			mdp4_wfd_pipe_queue(0, pipe);/* cndx = 0 */
 	}
 
-	/* write out DPP HSIC registers */
+/* NEED HSIC CODE
+	// write out DPP HSIC registers
 	if (pipe->flags & MDP_DPP_HSIC)
 		mdp4_hsic_update(pipe);
+NEED HSIC CODE */
+
 	if (!(pipe->flags & MDP_OV_PLAY_NOWAIT))
 		mdp4_iommu_unmap(pipe);
 	mdp4_stat.overlay_play[pipe->mixer_num]++;
