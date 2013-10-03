b7418485b52a641114a1dd900723b1dc46cc1f78
RaymanFX <raymanfx@gmail.com>
2013-10-03 15:41:51 +0200

    FIXME: revert ION api to MR1 mako

--- arch/arm/mach-msm/board-semc_fuji.c
+++ arch/arm/mach-msm/board-semc_fuji.c
@@ -131,7 +131,7 @@
 #include "pm-boot.h"
 #include "board-storage-common-a.h"
 
-#include <linux/msm_ion.h>
+#include <linux/ion.h>
 #include <mach/ion.h>
 #include <mach/msm_rtb.h>
 
@@ -4356,7 +4356,6 @@ static struct ion_platform_data ion_pdata = {
 			.size	= MSM_ION_MM_SIZE,
 			.memory_type = ION_SMI_TYPE,
 			.extra_data = (void *) &cp_mm_ion_pdata,
-			.priv  = &ion_mm_heap_device.dev,
 		},
 		{
 			.id     = ION_MM_FIRMWARE_HEAP_ID,
