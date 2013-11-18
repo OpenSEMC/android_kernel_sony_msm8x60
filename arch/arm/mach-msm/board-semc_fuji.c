94c0c91d35d63520734832bc6efd10fb2e24e75b
RaymanFX <raymanfx@gmail.com>
2013-11-18 22:55:03 +0100

    fuji: update board from CAF
    * taken from https://github.com/CyanogenMod/android_kernel_sony_msm8x60/commit/96b54bb538bf1bc00da766053c3f7b4a814ad8bd

--- arch/arm/mach-msm/board-semc_fuji.c
+++ arch/arm/mach-msm/board-semc_fuji.c
@@ -4004,6 +4004,12 @@ static struct msm_bus_vectors qseecom_clks_init_vectors[] = {
 		.ab = 0,
 	},
 	{
+		.src = MSM_BUS_MASTER_SPS,
+		.dst = MSM_BUS_SLAVE_SPS,
+		.ib = 0,
+		.ab = 0,
+	},
+	{
 		.src = MSM_BUS_MASTER_SPDM,
 		.dst = MSM_BUS_SLAVE_SPDM,
 		.ib = 0,
@@ -4017,6 +4023,12 @@ static struct msm_bus_vectors qseecom_enable_dfab_vectors[] = {
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ib = (492 * 8) * 1000000UL,
 		.ab = (492 * 8) *  100000UL,
+	},
+	{
+		.src = MSM_BUS_MASTER_SPS,
+		.dst = MSM_BUS_SLAVE_SPS,
+		.ib = (492 * 8) * 1000000UL,
+		.ab = (492 * 8) * 100000UL,
 	},
 	{
 		.src = MSM_BUS_MASTER_SPDM,
@@ -4034,11 +4046,38 @@ static struct msm_bus_vectors qseecom_enable_sfpb_vectors[] = {
 		.ab = 0,
 	},
 	{
+                .src = MSM_BUS_MASTER_SPS,
+                .dst = MSM_BUS_SLAVE_SPS,
+                .ib = 0,
+                .ab = 0,
+        },
+	{
 		.src = MSM_BUS_MASTER_SPDM,
 		.dst = MSM_BUS_SLAVE_SPDM,
 		.ib = (64 * 8) * 1000000UL,
 		.ab = (64 * 8) *  100000UL,
 	},
+};
+
+static struct msm_bus_vectors qseecom_enable_dfab_sfpb_vectors[] = {
+        {
+                .src = MSM_BUS_MASTER_SPS,
+                .dst = MSM_BUS_SLAVE_EBI_CH0,
+                .ib = (492 * 8) * 1000000UL,
+                .ab = (492 * 8) *  100000UL,
+        },
+        {
+                .src = MSM_BUS_MASTER_SPS,
+                .dst = MSM_BUS_SLAVE_SPS,
+                .ib = (492 * 8) * 1000000UL,
+                .ab = (492 * 8) *  100000UL,
+        },
+        {
+                .src = MSM_BUS_MASTER_SPDM,
+                .dst = MSM_BUS_SLAVE_SPDM,
+                .ib = (64 * 8) * 1000000UL,
+                .ab = (64 * 8) *  100000UL,
+        },
 };
 
 static struct msm_bus_paths qseecom_hw_bus_scale_usecases[] = {
@@ -4048,11 +4087,15 @@ static struct msm_bus_paths qseecom_hw_bus_scale_usecases[] = {
 	},
 	{
 		ARRAY_SIZE(qseecom_enable_dfab_vectors),
-		qseecom_enable_sfpb_vectors,
+		qseecom_enable_dfab_vectors,
 	},
 	{
 		ARRAY_SIZE(qseecom_enable_sfpb_vectors),
 		qseecom_enable_sfpb_vectors,
+	},
+	{
+		ARRAY_SIZE(qseecom_enable_dfab_sfpb_vectors),
+		qseecom_enable_dfab_sfpb_vectors,
 	},
 };
 
