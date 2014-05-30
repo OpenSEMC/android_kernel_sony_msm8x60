ae681e9e75227c921678facb04e1cc841d6c79ad
raymanfx <raymanfx@gmail.com>
2014-05-30 17:26:44 +0200

    media: videobuf2-msm-mem: fix deprecated ION handle

--- drivers/media/video/videobuf2-msm-mem.c
+++ drivers/media/video/videobuf2-msm-mem.c
@@ -186,15 +186,29 @@ int videobuf2_pmem_contig_user_get(struct videobuf2_contig_pmem *mem,
 	if (mem->phyaddr != 0)
 		return 0;
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
-	mem->ion_handle = ion_import_dma_buf(client, (int)mem->vaddr);
-	if (IS_ERR_OR_NULL(mem->ion_handle)) {
-		pr_err("%s ION import failed\n", __func__);
-		return PTR_ERR(mem->ion_handle);
-	}
-	rc = ion_map_iommu(client, mem->ion_handle, domain_num, 0,
-		SZ_4K, 0, (unsigned long *)&mem->phyaddr, &len, UNCACHED, 0);
-	if (rc < 0)
-		ion_free(client, mem->ion_handle);
+    mem->ion_handle = ion_import_dma_buf(client, (int)mem->vaddr);
+    if (IS_ERR_OR_NULL(mem->ion_handle)) {
+            pr_err("%s ION import failed\n", __func__);
+            return PTR_ERR(mem->ion_handle);
+    }
+    rc = ion_map_iommu(client, mem->ion_handle, CAMERA_DOMAIN, GEN_POOL,
+            SZ_4K, 0, (unsigned long *)&mem->phyaddr, &len, 0, 0);
+    if (rc < 0)
+            ion_free(client, mem->ion_handle);
+
+    rc = ion_handle_get_flags(client, mem->ion_handle, &ionflag);
+    if (rc) {
+            pr_err("%s: could not get flags for the handle\n", __func__);
+            return 0;
+    }
+    D("ionflag=%ld\n", ionflag);
+    vaddr = ion_map_kernel(client, mem->ion_handle);
+    if (IS_ERR_OR_NULL(vaddr)) {
+            pr_err("%s: could not get virtual address\n", __func__);
+            return 0;
+    }
+    mem->arm_vaddr = vaddr;
+    D("arm_vaddr=0x%lx\n", (unsigned long) mem->arm_vaddr);
 #elif CONFIG_ANDROID_PMEM
 	rc = get_pmem_file((int)mem->vaddr, (unsigned long *)&mem->phyaddr,
 					&kvstart, &len, &mem->file);
